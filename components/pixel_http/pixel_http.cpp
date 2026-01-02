c4917d54fdf61bddf57e9c71bc145dd0c8208e88
Thomas Steger <ts@thomas-steger.com>
2026-01-02 20:52:03 +0100

    Refactor PixelHttp to use AsyncWebServer
    Refactor PixelHttp to use AsyncWebServer and simplify request handling.

--- components/pixel_http/pixel_http.cpp
+++ components/pixel_http/pixel_http.cpp
@@ -1,37 +1,17 @@
 #include "pixel_http.h"
 
-using namespace esphome;
-using namespace esphome::web_server;
-
+namespace esphome {
 namespace pixel_http {
 
-bool PixelHttp::canHandle(WebRequest *request) {
-  return request->get_url() == "/pixels.bin";
-}
+static AsyncWebServer server(8080);
 
-void PixelHttp::handleRequest(WebRequest *request) {
-  if (this->light_ == nullptr) {
-    request->send(500, "text/plain", "Light not set");
-    return;
-  }
+void PixelHttp::setup() {
+  server.on("/pixels", HTTP_GET, [this](AsyncWebServerRequest *request) {
+    request->send(200, "application/json", "[]");
+  });
 
-  const uint16_t count = this->light_->size();
-  const size_t len = 2 + count * 3;
-
-  auto *buf = new uint8_t[len];
-  buf[0] = count & 0xFF;
-  buf[1] = (count >> 8) & 0xFF;
-
-  for (uint16_t i = 0; i < count; i++) {
-    auto c = this->light_->get_pixel_color(i);
-    buf[2 + i * 3 + 0] = c.red;
-    buf[2 + i * 3 + 1] = c.green;
-    buf[2 + i * 3 + 2] = c.blue;
-  }
-
-  request->send(200, "application/octet-stream", buf, len);
-  delete[] buf;
+  server.begin();
 }
 
 }  // namespace pixel_http
-
+}  // namespace esphome

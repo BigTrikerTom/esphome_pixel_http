20023331894dce16e7b663840512f41d9eebb881
Thomas Steger <ts@thomas-steger.com>
2026-01-02 20:51:22 +0100

    Refactor PixelHttp class to remove web server methods
    Removed web server handling from PixelHttp class.

--- components/pixel_http/pixel_http.h
+++ components/pixel_http/pixel_http.h
@@ -1,23 +1,20 @@
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/components/light/addressable_light.h"
 #include <ESPAsyncWebServer.h>
 
+namespace esphome {
 namespace pixel_http {
 
-class PixelHttp : public esphome::Component,
-                  public esphome::web_server::WebHandler {
+class PixelHttp : public Component {
  public:
-  void set_light(esphome::light::AddressableLight *light) {
-    this->light_ = light;
-  }
-
-  bool canHandle(esphome::web_server::WebRequest *request) override;
-  void handleRequest(esphome::web_server::WebRequest *request) override;
+  void set_light(light::AddressableLight *light) { light_ = light; }
+  void setup() override;
 
  protected:
-  esphome::light::AddressableLight *light_{nullptr};
+  light::AddressableLight *light_{nullptr};
 };
 
 }  // namespace pixel_http
-
+}  // namespace esphome

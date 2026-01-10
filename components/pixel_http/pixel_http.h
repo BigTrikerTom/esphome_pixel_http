1e2636aa4088fbf0dbdcd54549b0d3838b3ad5b6
Thomas Steger <ts@thomas-steger.com>
2026-01-10 20:31:28 +0100

    Integrate AsyncTCP and enhance PixelHttp class
    Added AsyncTCP support and updated PixelHttp class.

--- components/pixel_http/pixel_http.h
+++ components/pixel_http/pixel_http.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/light/addressable_light.h"
-#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
 
 namespace esphome {
 namespace pixel_http {
@@ -11,11 +11,16 @@ class PixelHttp : public Component {
  public:
   void set_light(light::AddressableLight *light) { light_ = light; }
   void setup() override;
+  void loop() override;
 
  protected:
   light::AddressableLight *light_{nullptr};
+  AsyncServer *server_{nullptr};
+  AsyncClient *client_{nullptr};
+  uint32_t last_update_{0};
+
+  void send_pixels_();
 };
 
 }  // namespace pixel_http
 }  // namespace esphome
-

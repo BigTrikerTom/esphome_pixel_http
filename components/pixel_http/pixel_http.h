1e69e85c5ea863612a0accf406965f1fbe69e2c0
Thomas Steger <ts@thomas-steger.com>
2026-01-10 20:36:22 +0100

    Replace AsyncTCP with WiFi for server and client

--- components/pixel_http/pixel_http.h
+++ components/pixel_http/pixel_http.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/light/addressable_light.h"
-#include <AsyncTCP.h>
+#include <WiFi.h>
 
 namespace esphome {
 namespace pixel_http {
@@ -15,8 +15,10 @@ class PixelHttp : public Component {
 
  protected:
   light::AddressableLight *light_{nullptr};
-  AsyncServer *server_{nullptr};
-  AsyncClient *client_{nullptr};
+
+  WiFiServer server_{9090};
+  WiFiClient client_;
+
   uint32_t last_update_{0};
 
   void send_pixels_();

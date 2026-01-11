805de5b677bac2615a2cdb6870513c7eab9fe292
Thomas Steger <ts@thomas-steger.com>
2026-01-11 22:54:03 +0100

    Change light state handling to use AddressableLight

--- components/pixel_http/pixel_http.h
+++ components/pixel_http/pixel_http.h
@@ -1,8 +1,6 @@
-
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/light/light_state.h"
 #include "esphome/components/light/addressable_light.h"
 
 #include "lwip/sockets.h"
@@ -13,17 +11,16 @@ namespace pixel_http {
 
 class PixelHttp : public Component {
  public:
-  void set_light(light::LightState *state) { state_ = state; }
+  void set_light(light::AddressableLight *light) { light_ = light; }
 
   void setup() override;
   void loop() override;
 
  protected:
-  light::LightState *state_{nullptr};
+  light::AddressableLight *light_{nullptr};
 
   int server_fd_{-1};
   int client_fd_{-1};
-
   uint32_t last_update_{0};
 
   void send_pixels_();

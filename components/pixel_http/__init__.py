cb4c93940e79db2782d0369db4c1b9532eb9f4ca
Thomas Steger <ts@thomas-steger.com>
2026-01-11 22:53:29 +0100

    Update light variable handling in PixelHttp component

--- components/pixel_http/__init__.py
+++ components/pixel_http/__init__.py
@@ -8,12 +8,12 @@ PixelHttp = pixel_http_ns.class_("PixelHttp", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(PixelHttp),
-    cv.Required(CONF_LIGHT_ID): cv.use_id(light.LightState),
+    cv.Required(CONF_LIGHT_ID): cv.use_id(light.AddressableLight),
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    light_state = await cg.get_variable(config[CONF_LIGHT_ID])
-    cg.add(var.set_light(light_state))
+    light_var = await cg.get_variable(config[CONF_LIGHT_ID])
+    cg.add(var.set_light(light_var))

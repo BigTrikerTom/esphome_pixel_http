1d37c50f57fbd2cf5f83e8ab9d6ee48e9a4fc2ff
Thomas Steger <ts@thomas-steger.com>
2026-01-11 18:06:19 +0100

    Change light state type from AddressableLightState to LightState

--- components/pixel_http/pixel_http.h
+++ components/pixel_http/pixel_http.h
@@ -1,8 +1,9 @@
+
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/components/light/light_state.h"
 #include "esphome/components/light/addressable_light.h"
-#include "esphome/components/light/addressable_light_state.h"
 
 #include "lwip/sockets.h"
 #include "lwip/netdb.h"
@@ -12,13 +13,13 @@ namespace pixel_http {
 
 class PixelHttp : public Component {
  public:
-  void set_light(light::AddressableLightState *state) { state_ = state; }
+  void set_light(light::LightState *state) { state_ = state; }
 
   void setup() override;
   void loop() override;
 
  protected:
-  light::AddressableLightState *state_{nullptr};
+  light::LightState *state_{nullptr};
 
   int server_fd_{-1};
   int client_fd_{-1};

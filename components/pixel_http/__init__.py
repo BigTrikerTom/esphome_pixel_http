f329e982d0dddd1d07b2bead0251e70282f65a72
Thomas Steger <ts@thomas-steger.com>
2026-01-11 17:55:40 +0100

    Update light component reference in pixel_http

--- components/pixel_http/__init__.py
+++ components/pixel_http/__init__.py
@@ -1,19 +1,19 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import web_server
 from esphome.const import CONF_ID, CONF_LIGHT_ID
+from esphome.components import light
 
 pixel_http_ns = cg.esphome_ns.namespace("pixel_http")
 PixelHttp = pixel_http_ns.class_("PixelHttp", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(PixelHttp),
-    cv.Required(CONF_LIGHT_ID): cv.use_id(cg.Component),
+    cv.Required(CONF_LIGHT_ID): cv.use_id(light.AddressableLightState),
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    light = await cg.get_variable(config[CONF_LIGHT_ID])
-    cg.add(var.set_light(light))
 
+    light_state = await cg.get_variable(config[CONF_LIGHT_ID])
+    cg.add(var.set_light(light_state))

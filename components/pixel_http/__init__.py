dbb939f745e115df39b05f5cde3b338294aea706
Thomas Steger <ts@thomas-steger.com>
2026-01-12 20:02:03 +0100

    Define CONFIG_SCHEMA for PixelHTTPComponent
    Add YAML schema configuration for PixelHTTPComponent.

--- components/pixel_http/__init__.py
+++ components/pixel_http/__init__.py
@@ -9,6 +9,7 @@ CONF_PORT = "port"
 pixel_http_ns = cg.esphome_ns.namespace("pixel_http")
 PixelHTTPComponent = pixel_http_ns.class_("PixelHTTPComponent", cg.Component)
 
+# YAML Schema für deine Komponente
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(PixelHTTPComponent),
     cv.Required(CONF_NUM_LEDS): cv.int_,
@@ -17,6 +18,7 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
+    # Instanzierung mit num_leds, pin und Port
     var = cg.new_Pvariable(
         config[CONF_ID],
         config[CONF_NUM_LEDS],
@@ -24,4 +26,3 @@ async def to_code(config):
         config[CONF_PORT],
     )
     await cg.register_component(var, config)
-    

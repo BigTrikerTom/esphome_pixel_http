53e69fcbe92589e8eed83082125b123962712d76
Thomas Steger <ts@thomas-steger.com>
2026-01-12 18:00:41 +0100

    Implement PixelHTTPComponent with config validation
    Added PixelHTTPComponent with configuration schema and async code generation.

--- components/pixel_http/__init__.py
+++ components/pixel_http/__init__.py
@@ -1,3 +1,23 @@
-# __init__.py
-# Registrierung für den PixelHTTP Component Namespace
-from esphome.components import sensor, light
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+CONF_NUM_LEDS = "num_leds"
+CONF_PIN = "pin"
+
+pixel_http_ns = cg.esphome_ns.namespace("pixel_http")
+PixelHTTPComponent = pixel_http_ns.class_("PixelHTTPComponent", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(PixelHTTPComponent),
+    cv.Required(CONF_NUM_LEDS): cv.int_,
+    cv.Required(CONF_PIN): cv.int_,
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(
+        config[CONF_ID],
+        config[CONF_NUM_LEDS],
+        config[CONF_PIN],
+    )
+    await cg.register_component(var, config)

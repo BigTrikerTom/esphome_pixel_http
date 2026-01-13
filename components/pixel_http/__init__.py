6edda002018754dd54e334e97081f9f713e0f224
Thomas Steger <ts@thomas-steger.com>
2026-01-13 02:38:47 +0100

    Update __init__.py

--- components/pixel_http/__init__.py
+++ components/pixel_http/__init__.py
@@ -9,16 +9,14 @@ CONF_PORT = "port"
 pixel_http_ns = cg.esphome_ns.namespace("pixel_http")
 PixelHTTPComponent = pixel_http_ns.class_("PixelHTTPComponent", cg.Component)
 
-# YAML Schema für deine Komponente
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(PixelHTTPComponent),
-    cv.Required(CONF_NUM_LEDS): cv.int_,
-    cv.Required(CONF_PIN): cv.int_,
+    cv.Required(CONF_NUM_LEDS): cv.int_range(min=1, max=4096),
+    cv.Required(CONF_PIN): cv.int_range(min=0, max=39),
     cv.Optional(CONF_PORT, default=80): cv.port,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    # Instanzierung mit num_leds, pin und Port
     var = cg.new_Pvariable(
         config[CONF_ID],
         config[CONF_NUM_LEDS],
